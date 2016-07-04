5385801712b72c6178a967a2cb9600edd2295d2e
KharamanV <kharaman.vova@idev.bz>
2016-07-04 16:37:02 +0300

    Project structure is changed. Mustache template engine is added, but isn't installed

---
+++ application/controllers/News.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class News extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->model('NewsModel');
+        $data['news'] = $this->NewsModel->allNews();
+        $this->load->view('news/all', $data);
+    }
+    public function add()
+    {
+        $this->load->view('news/add');
+    }
+
+    public function mustache()
+    {
+        echo "string";
+    }
+    
+
+}
