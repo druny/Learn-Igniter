23061dc94bd20f387c87c6a666fc83fc185d0220
KharamanV <kharaman.vova@idev.bz>
2016-07-07 12:19:12 +0300

    Mustache + composer

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://learn-igniter.su/';
+$config['base_url'] = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . '/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://learn-igniter.su/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -137,7 +137,7 @@ $config['subclass_prefix'] = 'MY_';
 |	autoloading (application/config/autoload.php)
 */
 
-$config['composer_autoload'] = TRUE;
+$config['composer_autoload'] = APPPATH . '../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
